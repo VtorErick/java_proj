89a63b1a2b916d3bd4c80fc0f183304d1eb5cd58
Victor Acosta <victorerickad@gmail.com>
2023-01-14 15:10:24 -0600

    fix tests

--- src/test/java/com/wizeline/onsite/HelloWorldTests.java
+++ src/test/java/com/wizeline/onsite/HelloWorldTests.java
@@ -1,8 +1,9 @@
 package com.wizeline.onsite;
 
+import org.junit.jupiter.api.Test;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import org.junit.Test;
 
 public class HelloWorldTests {
 
